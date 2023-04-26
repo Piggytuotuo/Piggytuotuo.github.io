70c3df972212f070cf7f1cea28623903b3862ded
piggytuotuo <154254564@qq.com>
2023-04-26 22:24:16 +0800

    topk

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230421.111248';
+const cacheName = 'chirpy-20230426.222315';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
