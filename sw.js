e9395856517e8baf70a60c3dc95e65d3db024eb1
piggytuotuo <154254564@qq.com>
2023-04-27 17:32:58 +0800

    recursion

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230427.130001';
+const cacheName = 'chirpy-20230427.140619';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
