8ad1ff2e40d47e6660a406773ec0866d60e5519f
piggytuotuo <154254564@qq.com>
2023-06-08 12:11:59 +0800

    update

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230601.220129';
+const cacheName = 'chirpy-20230608.121048';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
