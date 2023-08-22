be047541d2346e70ddb3d6a57587245f0aa6f428
lyf <154254564@qq.com>
2023-08-22 21:38:40 +0800

    update_git

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230821.071700';
+const cacheName = 'chirpy-20230822.213735';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
