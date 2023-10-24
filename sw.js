c1861b1abf0658883d86ca1fa34e158fdae8f807
lyf <154254564@qq.com>
2023-10-24 22:07:54 +0800

    latex

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230822.221135';
+const cacheName = 'chirpy-20231024.220440';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
