f60048a26cf0920f5a968af45c2c71764c426015
piggytuotuo <154254564@qq.com>
2024-01-24 15:04:31 +0800

    0124

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240124.144253';
+const cacheName = 'chirpy-20240124.150048';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
