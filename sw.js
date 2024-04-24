cdfe51f52ae62c4ba6b88e691217297c275b8230
lyf <154254564@qq.com>
2024-04-24 15:59:27 +0800

    0424

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240422.120929';
+const cacheName = 'chirpy-20240424.155804';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
